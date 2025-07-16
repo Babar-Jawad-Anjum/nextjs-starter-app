48b798de9e76f0802171c3f1e76a6f8053fcd08f
Babar-Jawad-Anjum <89509895+Babar-Jawad-Anjum@users.noreply.github.com>
2025-07-16 19:09:26 +0500

    feat: commit message rules updated

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // new features
+        'fix', // bug fixes
+        'chore', // tooling, infra
+        'docs', // documentation
+        'refactor', // code improvements (no feature/bug)
+        'test', // adding tests
+        'style', // formatting, missing semicolons, etc.
+        'ci', // CI/CD related
+        'perf', // performance improvements
+      ],
+    ],
+  },
 }
